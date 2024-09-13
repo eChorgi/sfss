7707939a76cf82fd886e4f5e55c1222b12a3480f
eChorgi <844693677@qq.com>
2024-09-13 19:20:47 +0800

    test

--- lib/roughter.dart
+++ lib/roughter.dart
@@ -2,3 +2,24 @@ export 'pages/login/page_login.dart';
 export 'pages/welcome/page_welcome.dart';
 export 'pages/register/page_register.dart';
 export 'pages/home/page_home.dart';
+
+import 'pages/login/page_login.dart';
+import 'pages/welcome/page_welcome.dart';
+import 'pages/register/page_register.dart';
+import 'pages/home/page_home.dart';
+import 'package:flutter/material.dart';
+
+Map<String, Widget Function(BuildContext)> appRoutes = {
+  '/':(context) {
+    return const PageWelcome();
+  },
+  '/login': (context) {
+    return const PageLogin();
+  },
+  '/register': (context) {
+    return const PageRegister();
+  },
+  '/home': (context) {
+    return const PageHome();
+  },
+};
