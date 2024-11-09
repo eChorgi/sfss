7c0600478715231fded16e17a3266bdcc9dc5258
Zeworlrod <1961338216@qq.com>
2024-11-10 07:17:36 +0800

    新增立冬页面及相关颜色定义

--- lib/styles/sfss_style.dart
+++ lib/styles/sfss_style.dart
@@ -13,5 +13,35 @@ class SfssStyle{
   const Color(0xFFD3D3D3),
   const Color(0xFF8BA97B),
   const Color(0xFFDA9F61),
+  ];
+
+  List<List<Color>> solarTermColors = [
+  [Color(0xFF85A193), Color(0xFF9DBBAC), Color(0xFFB7D3C8)],
+  [Color(0xFF576F4B), Color(0xFF7C9253), Color(0xFFABBC81)],
+  [Color(0xFFBB7C34), Color(0xFFD09D4A), Color(0xFFF2C35A)],
+  [Color(0xFF91695A), Color(0xFFA77061), Color(0xFFB4836E)],
+  [Color(0xFF7A836F), Color(0xFF909079), Color(0xFF9C9B84)],
+  [Color(0xFF755580), Color(0xFFA17EB3), Color(0xFFB6A0C8)],
+  
+  [Color(0xFFD1312B), Color(0xFFD65539), Color(0xFFDC744C)],
+  [Color(0xFF966336), Color(0xFFC58847), Color(0xFFCAA34D)],
+  [Color(0xFF918F61), Color(0xFFAAA689), Color(0xFFBEBF99)],
+  [Color(0xFF782F2E), Color(0xFF883A35), Color(0xFFA24539)],
+  [Color(0xFF19365D), Color(0xFF21446B), Color(0xFF2A4F77)],
+  [Color(0xFF6F8D6D), Color(0xFF86A182), Color(0xFF9EB296)],
+  
+  [Color(0xFF3B58A1), Color(0xFF6075B4), Color(0xFF7691C9)],
+  [Color(0xFFAE7CAC), Color(0xFFC594B9), Color(0xFFDEB8D2)],
+  [Color(0xFFD1C6A4), Color(0xFFDCD4BA), Color(0xFFE9E3D3)],
+  [Color(0xFFB5BBA8), Color(0xFFBFC9B7), Color(0xFFCCD6C5)],
+  [Color(0xFF222B46), Color(0xFF25355D), Color(0xFF1D3E70)],
+  [Color(0xFF937E6D), Color(0xFFB8A78D), Color(0xFFD0C0A7)],
+  
+  [Color(0xFF602F31), Color(0xFF7B4C50), Color(0xFF966B6E)],
+  [Color(0xFF8FA9C8), Color(0xFFA6B9D9), Color(0xFFBFD3E1)],
+  [Color(0xFFA44D55), Color(0xFFB5626A), Color(0xFFC28A91)],
+  [Color(0xFF343531), Color(0xFF484640), Color(0xFF58544A)],
+  [Color(0xFF1D212B), Color(0xFF414754), Color(0xFF59636D)],
+  [Color(0xFF70585C), Color(0xFF97868E), Color(0xFF978C9A)],
 ];
 }
