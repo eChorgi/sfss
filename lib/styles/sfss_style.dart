d2579080bc4205987220c115882d17cbb04d78f7
旺旺君贝 <1961338216@qq.com>
2024-11-10 06:38:10 +0800

    新增颜色定义和徽章等级颜色数组

--- lib/styles/sfss_style.dart
+++ lib/styles/sfss_style.dart
@@ -4,4 +4,14 @@ class SfssStyle{
   static String mainFont = "又又意宋";
   static Color mainRed = const Color(0xFF791414);
   static Color mainGrey = const Color(0xFF4A5568);
+  static Color weakRed = const Color(0xFF882F2F);
+  static Color weakGrey = const Color(0xFFA4AAB3);
+  static Color mainWhite = const Color(0xFFFBFAFA);
+
+  //颜色数组
+  List<Color> badgeLevelColors = [
+  const Color(0xFFD3D3D3),
+  const Color(0xFF8BA97B),
+  const Color(0xFFDA9F61),
+];
 }
